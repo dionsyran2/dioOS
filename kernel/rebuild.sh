6fddc00ddbadf5d45d39d7355e17dc222c77c344
dionsyran2 <72520980+dionsyran2@users.noreply.github.com>
2025-04-21 16:04:12 +0300

    Delete kernel directory

--- kernel/rebuild.sh
+++
@@ -1,5 +0,0 @@
-cd ~/dioOS/gnu-efi
-make bootloader
-cd ~/dioOS/kernel
-make kernel
-make buildimg
--- kernel/src/start_up.asm
+++
@@ -1,9 +0,0 @@
-section .text
-global ap_startup
-
-ap_startup:
-    mov rsp, [ap_stack]  ; Set stack pointer
-    extern ap_main
-    call ap_main
-    hlt
-    jmp $
